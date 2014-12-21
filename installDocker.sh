d4881f76a55a6885732327b04f8f4cd1070dafea
jlamby <lamby.julien@gmail.com>
2014-12-21 15:28:50 +0100

    Adding docker

---
+++ installDocker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "deb http://http.debian.net/debian wheezy-backports main" >> /etc/apt/sources.list
+apt-get update
+apt-get install -t wheezy-backports linux-image-amd64
+
+curl -sSL https://get.docker.com/ | sh
+
+# Add the docker group if it doesn't already exist.
+$ sudo groupadd docker
+
+# Add the connected user "${USER}" to the docker group.
+# Change the user name to match your preferred user.
+# You may have to logout and log back in again for
+# this to take effect.
+$ sudo gpasswd -a ${USER} docker
+
+# Restart the Docker daemon.
+$ sudo service docker restart
