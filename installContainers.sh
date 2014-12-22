9bf7881f5530e3120c5f9973c56920078d654f50
jlamby <lamby.julien@gmail.com>
2014-12-22 12:23:07 +0100

    Adding data container for logs

--- installContainers.sh
+++ installContainers.sh
@@ -6,6 +6,9 @@ if [ $# -gt 0 ]; then
 	basePath=$1"/"
 fi
 
+# build data volume containers
+docker build -t "logs" $basePath"docker-logs"
+
 # build Apache-PHP container
 docker build -t "apache-php" $basePath"docker-apache"
 
--- runContainers.sh
+++ runContainers.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker run -d -i -t -p 80:80 --name="apache" apache-php
+# run data volume containers
+docker run -d -i -t --name LOGS logs
+
+# run normal containers
+docker run -d -i -t -p 80:80    --name apache       --volumes-from LOGS apache-php
